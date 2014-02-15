1e694b9df2a17e79950d1ce16ecb390836404ba9
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-02-15 14:30:22 +0900

    Implement Judge#float_judge

--- lib/clicoder/judge.rb
+++ lib/clicoder/judge.rb
@@ -7,5 +7,16 @@ module Clicoder
     def diff_judge(file1, file2)
       File.read(file1) == File.read(file2)
     end
+
+    def float_judge(file1, file2, absolute_error)
+      lines1 = File.read(file1).split($/).map(&:strip)
+      floats1 = lines1.map{ |line| line.split(/\s+/).map(&:to_f) }.flatten
+      lines2 = File.read(file2).split($/).map(&:strip)
+      floats2 = lines2.map{ |line| line.split(/\s+/).map(&:to_f) }.flatten
+      floats1.zip(floats2).each do |float1, float2|
+        return false if (float1 - float2).abs >= absolute_error
+      end
+      true
+    end
   end
 end
--- spec/judge_spec.rb
+++ spec/judge_spec.rb
@@ -9,26 +9,55 @@ module Clicoder
   describe Judge do
     let(:judge) { Judge.new }
 
-    before(:all) do
-      # input for judge
-      @input = Tempfile.new('clicoder')
-      @input.write([1, 2, 3].join("\n"))
-      @input.close
-      @correct_output = Tempfile.new('clicoder')
-      @correct_output.write([1, 2, 3].join("\n"))
-      @correct_output.close
-      @wrong_output = Tempfile.new('clicoder')
-      @wrong_output.write([1, 2, 4].join("\n"))
-      @wrong_output.close
-    end
-
     describe '#diff_judge' do
+      before(:all) do
+        # input for judge
+        @input = Tempfile.new('clicoder')
+        @input.write([1, 2, 3].join("\n"))
+        @input.close
+        @correct_output = Tempfile.new('clicoder')
+        @correct_output.write([1, 2, 3].join("\n"))
+        @correct_output.close
+        @wrong_output = Tempfile.new('clicoder')
+        @wrong_output.write([1, 2, 4].join("\n"))
+        @wrong_output.close
+      end
+
       it 'returns true when the contents of two files are the same' do
         expect(judge.diff_judge(@input.path, @correct_output.path)).to be_true
       end
 
       it 'returns false when the contents of two files are different' do
         expect(judge.diff_judge(@input.path, @wrong_output.path)).to be_false
+      end
+    end
+
+    describe '#float_judge' do
+      before(:all) do
+        @input = Tempfile.new('clicoder')
+        @input.write(<<-EOS)
+        0.11 0.11
+        0.13 0.13
+        EOS
+        @input.close
+        @output = Tempfile.new('clicoder')
+        @output.write(<<-EOS)
+        0.12 0.12
+        0.12 0.12
+        EOS
+        @output.close
+      end
+
+      subject { judge.float_judge(@input.path, @output.path, abs_error) }
+
+      context 'when diff is less than allowed absolute error' do
+        let(:abs_error) { 10**(-1) }
+        it { should be_true }
+      end
+
+      context 'when diff is less than allowed absolute error' do
+        let(:abs_error) { 10**(-2) }
+        it { should be_false }
       end
     end
   end
