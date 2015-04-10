d9cd20fcf6b1a2eb1004d52d5e0690e330eb7b61
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2015-04-11 08:53:47 +0900

    Fix feature specs

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -3,3 +3,10 @@ require 'webmock/cucumber'
 # TODO: DRY. see spec/spec_helper.rb
 GEM_ROOT = Gem::Specification.find_by_name('clicoder').gem_dir
 FIXTURE_DIR = GEM_ROOT + '/fixtures'
+
+Before do
+  # TODO: DRY. see spec/spec_helper.rb
+  stub_request(:get, "http://samplesite.com/sample_problem.html").
+    to_return(status: 200,
+              body: File.read("#{FIXTURE_DIR}/sample_problem.html"))
+end
